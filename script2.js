1f95397a190afc7bc719182e09499b9f9469154c
tarik-fassil <140429163+tarik-fassil@users.noreply.github.com>
2023-07-25 19:24:33 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,13 @@
+ScrollReveal({ 
+    reset: true ,
+    distance:'60px',
+    duration:2500,
+    delay:350,
+});
+ScrollReveal().reveal('.featured-text-1', { delay: 300, origin: 'left'});
+ScrollReveal().reveal('.featured-text-2', { delay: 300, origin: 'right'});
+ScrollReveal().reveal('.img-about,.logo', { delay: 400, origin: 'right'});
+ScrollReveal().reveal('.tex-about, .section-heading', { delay: 500, origin: 'left'});
+ScrollReveal().reveal('.list', { delay: 300, origin: 'top'});
+ScrollReveal().reveal('.project', { delay: 200, distance:'40px'});
+ScrollReveal().reveal('.service', { delay: 300, origin: 'right'});
