aec7141ca4d39c33fbc6c1c118cd68953203b04b
tarik-fassil <140429163+tarik-fassil@users.noreply.github.com>
2023-07-25 19:31:21 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,7 +7,8 @@ ScrollReveal({
 ScrollReveal().reveal('.featured-text-1', { delay: 300, origin: 'left'});
 ScrollReveal().reveal('.featured-text-2', { delay: 300, origin: 'right'});
 ScrollReveal().reveal('.img-about,.logo', { delay: 400, origin: 'right'});
-ScrollReveal().reveal('.tex-about, .section-heading', { delay: 500, origin: 'left'});
+ScrollReveal().reveal('.tex-about', { delay: 500, origin: 'left'});
 ScrollReveal().reveal('.list', { delay: 300, origin: 'top'});
 ScrollReveal().reveal('.project', { delay: 200, distance:'40px'});
+ScrollReveal().reveal('.section-heading', { delay: 200, origin: 'left', distance:'30px'});
 ScrollReveal().reveal('.service', { delay: 300, origin: 'right'});
